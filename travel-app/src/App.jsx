02381445d198fdc6105e4d48f189f1f2ef7a78fb
Nixon Cam <nixon.cam997@gmail.com>
2025-10-08 14:34:14 +1300

    Add authentication context and login functionality

--- travel-app/src/App.jsx
+++ travel-app/src/App.jsx
@@ -1,18 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return <div><h1>Home Page</h1><Link to="/about">Go to About</Link></div>;
+}
+
+function About() {
+  return <div><h1>About Page</h1><Link to="/">Go to Home</Link></div>;
+}
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
